341eaa05abd72e4a9838327621fa054ff0013dc2
iago silva ambrosio <iago_ambrosio@outlook.com>
2023-10-09 21:58:38 -0300

    Colocando mock de validação de usuário, recurso e método

--- api/app/index.js
+++ api/app/index.js
@@ -6,11 +6,20 @@ app.use(express.urlencoded({extended:true}))
 const moduler = require('./src/routes/default')
 const mainPaths = ["/user","/application","/tenant"]
 // a nível de rota, o use recebe o caminho e manda para o Route do express
-app.get(mainPaths, moduler)
-app.post(mainPaths, moduler)
-app.put(mainPaths, moduler)
-app.delete(mainPaths, moduler)
-app.patch(mainPaths, moduler)
+
+app.all(mainPaths, (req, res, next) => {
+    console.log('Futura validação com a classe Validator')
+    next()
+       }, (req,res,next)=>
+       {
+res.send('Se tudo ocorrer como esperado, aqui vai uma resposta de erro, se não ele segue para o próximo roteador')
+    }
+)
+
+
+app.all(mainPaths, moduler)
+
+
 console.log(process.env.ENV)
 //connect prisma
 // infra bind and listener
